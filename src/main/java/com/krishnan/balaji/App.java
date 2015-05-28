4ca17e49ec77290b0baad86a8fbedb1a59094f4a
Balaji Krishnan <balaji2k9_Srec14@ymail.com>
2015-05-28 22:55:37 +0530

    Included code to try bean scopes

--- src/main/java/com/krishnan/balaji/App.java
+++ src/main/java/com/krishnan/balaji/App.java
@@ -5,10 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
+import com.krishnan.balaji.model.Point;
+
 public class App 
 {
 	 final static Logger logger = LoggerFactory.getLogger(App.class);
@@ -16,14 +14,36 @@ public class App
     public static void main( String[] args )
     {
     	 logger.info("Initializing Spring context.");
-         
-         AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("/context.xml");
-         
-         logger.info("Spring context initialized.");
-
-         Greeter greetings = (Greeter) applicationContext.getBean("greeter");
-
+         AbstractApplicationContext context = new ClassPathXmlApplicationContext("/context.xml");
+         Greeter greetings = (Greeter) context.getBean("greeter");
          logger.info("message='" + greetings.getMessage() + "'");
-         applicationContext.close();
+         tryScopes(context);
+         
+         context.close();
+         logger.info("Application context closed");
     }
+
+	private static void tryScopes(AbstractApplicationContext context) {
+		//point is defined as prototype in context
+		logger.info("loading prototype scoped point bean");
+		Point pointOne = context.getBean("point",Point.class);
+        Point pointTwo = context.getBean("point",Point.class);
+        logger.info("pointOne: " + pointOne.toString());
+        logger.info("pointTwo: " + pointTwo.toString());
+        logger.info("Updating pointOne's x value to 120");
+        pointOne.setX(120);
+        logger.info("pointOne: " + pointOne.toString());
+        logger.info("pointTwo: " + pointTwo.toString());
+
+        //somePoint is defined as singleton(default) inside context
+        logger.info("loading singleton scoped point bean");
+        Point pointThree = context.getBean("somePoint",Point.class);
+        Point pointFour = context.getBean("somePoint",Point.class);
+        logger.info("pointThree: " + pointThree.toString());
+        logger.info("pointFour: " + pointFour.toString());
+        logger.info("Updating pointThree's x value to 120");
+        pointThree.setX(120);
+        logger.info("pointThree: " + pointThree.toString());
+        logger.info("pointFour: " + pointFour.toString());        
+	}
 }
